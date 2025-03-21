15a51ce758fcba122c6ffe47fc2a7b59dd378bef
aig77 <arturo.igg@proton.me>
2025-03-21 00:33:41 -0400

    add auto garage collection

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,17 +1,30 @@
-{ config, inputs, ... }: {
+{ config, inputs, ... }: 
 
+{
   nixpkgs.config = {
     allowUnfree = true;
     allowBroken = true;
   };
-
-  nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
-    
-    # for installing Hyprland flake
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = ["nix-command" "flakes"];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };   
+    gc = {
+      automatic = true;
+      persistent = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
   };
-
 }
 
