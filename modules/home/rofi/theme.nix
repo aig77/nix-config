6c4fe1e62b378a3e7daf348e02780443f7aebb28
aig77 <arturo.igg@proton.me>
2025-04-05 15:59:13 -0400

    select wallpaper from stylix, hexToRgb for background

--- modules/home/rofi/theme.nix
+++ modules/home/rofi/theme.nix
@@ -15,7 +15,27 @@
     border = colors.base0C;
     selected1 = colors.base0D;
     selected2 = colors.base0E;
-    wallpaperPath = "~/nix/themes/sakura-midnight-train-thumb.jpg";
+    wallpaperPath = "${toString config.stylix.image}";
+    hexToRgb = hex: 
+      let
+        # Helper function to convert a 2-character hex string to a decimal integer manually
+        hexToDecimal = hexStr:
+          let
+            hexMap = { "0" = 0; "1" = 1; "2" = 2; "3" = 3; "4" = 4; "5" = 5; "6" = 6; "7" = 7; "8" = 8; "9" = 9;
+                      "a" = 10; "b" = 11; "c" = 12; "d" = 13; "e" = 14; "f" = 15; };
+            # Convert each character of the hex string to decimal and compute its contribution
+            hexStr1 = builtins.substring 0 1 hexStr;
+            hexStr2 = builtins.substring 1 1 hexStr;
+          in
+            (hexMap.${hexStr1} * 16) + hexMap.${hexStr2};
+        red = hexToDecimal (builtins.substring 1 2 hex);
+        green = hexToDecimal (builtins.substring 3 2 hex);
+        blue = hexToDecimal (builtins.substring 5 2 hex);
+      in [red green blue];
+      dark-rgb = hexToRgb dark;
+      dark-r = toString (builtins.elemAt dark-rgb 0);
+      dark-g = toString (builtins.elemAt dark-rgb 1);
+      dark-b = toString (builtins.elemAt dark-rgb 2);
   in {
     "configuration" = {
       modi = "drun,filebrowser,window,run";
@@ -42,8 +62,7 @@
       border = mkLiteral "2px";
       border-radius = mkLiteral "32px"; # 40
       border-color = mkLiteral "${border}";
-      #background-color = mkLiteral "rgba(0, 0, 0, 0.4)";
-      background-color = mkLiteral "${dark}";
+      background-color = mkLiteral "rgba(${dark-r}, ${dark-g}, ${dark-b}, 0.4)"; 
     };
 
     "mainbox" = {
