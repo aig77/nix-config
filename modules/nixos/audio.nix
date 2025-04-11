a962291ee548f323bf205b38482e5dc1aed384e0
aig77 <arturo.igg@proton.me>
2025-04-11 18:19:41 -0400

    add another audio source that has the rnnoise filter applied

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -1,4 +1,45 @@
-{
+{ pkgs, ... }: 
+# adds another audio source that has rnnoise filter applied
+let
+  pw_rnnoise_config = {
+    "context.modules" = [
+      {
+        "name" = "libpipewire-module-filter-chain";
+        "args" = {
+          "node.description" = "Noise Canceling source";
+          "media.name" = "Noise Canceling source";
+          "filter.graph" = {
+            "nodes" = [
+              {
+                "type" = "ladspa";
+                "name" = "rnnoise";
+                "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                "label" = "noise_suppressor_mono";
+                "control" = {
+                  "VAD Threshold (%)" = 50;
+                  "VAD Grace Period (ms)" = 20;
+                  "Retroactive VAD Grace (ms)" = 0;
+                };
+              }
+            ];
+          };
+          "capture.props" = {
+            "node.name" = "effect_input.rnnoise";
+            "node.passive" = true;
+            "audio.channels" = 1;
+            "audio.rate" = 48000;
+          };
+          "playback.props" = {
+            "node.name" = "effect_output.rnnoise";
+            "media.class" = "Audio/Source";
+            "audio.channels" = 1;
+            "audio.rate" = 48000;
+          };
+        };
+      }
+    ];
+  };
+in {
   security.rtkit.enable = true;
   # <= 24.11
   #hardware.pulseaudio.enable = false;
@@ -14,4 +55,7 @@
     jack.enable = true;
     wireplumber.enable = true;
   };
+  
+  # to fix staticy and sensitive mic 
+  services.pipewire.extraConfig.pipewire."99-input-denoising" = pw_rnnoise_config;
 }
