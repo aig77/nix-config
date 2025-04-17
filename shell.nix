cf77612683231d1f709c2c3e2089212f9705af8c
Arturo <29128253+aig77@users.noreply.github.com>
2025-04-16 22:55:15 -0400

    add some rust packages

--- shell.nix
+++ shell.nix
@@ -1,8 +1,16 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   packages = with pkgs; [
+    # Python
     (python3.withPackages (p: with p; [ numpy requests pandas ]))
+
+    # Rust
     rustc
+    cargo
+    rust-analyzer
+    clippy
+
+    # Other
     go
     zig
   ];
