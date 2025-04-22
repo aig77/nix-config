fad760c0136dba2467b16caae152b21b77aa8620
aig77 <git_lunchroom670@slmails.com>
2025-04-22 00:44:20 -0400

    basic nvidia config for desktop

---
+++ modules/nixos/nvidia.nix
@@ -0,0 +1,24 @@
+{
+  services.xserver.videoDrivers = [ "nvidia" ];
+  
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+  ];
+  
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "nvidia"; # Hardware video acceleration
+    GDM_BACKEND = "nvidia-drm"; # Graphics backend for Wayland
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # Use nvidia driver for GLX
+  };
+
+  nixpkgs.config = {
+    nvidia.acceptLicense = true;
+    allowUnfree = true;
+  };
+
+  hardware.nvidia = {
+    open = false;
+    nvidiaSettings = true; # Nvidia settings utility
+    modesetting.enable = true; # Required for Wayland
+  };
+}
