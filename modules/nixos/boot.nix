e66dbead8d2840f072bcb8791e77ff682e541160
aig77 <git_lunchroom670@slmails.com>
2025-04-22 00:44:47 -0400

    update boot config for desktop

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }: {
   boot = {
     loader = {
+      efi = {
+        canTouchEfiVariables = true;
+      };
       grub = {
         enable = true;
-        device = "/dev/sda";
+        #device = "/dev/sda";
+        devices = [ "nodev" ];
+        efiSupport = true; 
         useOSProber = true;
       };
     };
