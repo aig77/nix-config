e281e889ca61dbb381c742d0a92e10ae766fff16
Arturo <29128253+aig77@users.noreply.github.com>
2025-04-22 20:52:51 -0400

    fix monitors

--- modules/home/system/hyprland/monitors.nix
+++ modules/home/system/hyprland/monitors.nix
@@ -1,4 +1,9 @@
-{ config, ... }: {
+{ config, ... }: 
+let 
+  m1 = "DP-2";
+  m2 = "DP-3";
+in 
+{
   #wayland.windowManager.hyprland = {
   #  settings = { monitor = [ ",preferred,auto,auto" ]; };
   #};
@@ -7,20 +12,20 @@
     if (config.var.configName == "desktop") then {
       settings = {
         monitor = [
-          "DP-2, 2560x1440@300, 0x0, 1, bitdepth, 10"
-          "DP-1, 2560x1440@144, 2560x0, 1, bitdepth, 10"
+          "${m1}, 2560x1440@300, 0x0, 1, bitdepth, 10"
+          "${m2}, 2560x1440@144, 2560x0, 1, bitdepth, 10"
         ];
 
         workspace = [
-          "1, monitor:DP-2"
-          "2, monitor:DP-2"
-          "3, monitor:DP-2"
-          "4, monitor:DP-2"
+          "1, monitor:${m1}"
+          "2, monitor:${m1}"
+          "3, monitor:${m1}"
+          "4, monitor:${m1}"
 
-          "5, monitor:DP-1"
-          "6, monitor:DP-1"
-          "7, monitor:DP-1"
-          "8, monitor:DP-1"
+          "5, monitor:${m2}"
+          "6, monitor:${m2}"
+          "7, monitor:${m2}"
+          "8, monitor:${m2}"
         ];
       };
     } else {
