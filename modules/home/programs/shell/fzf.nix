5700cc90bf010cf12cfda29cf9b354bc71c594e1
aig77 <git_lunchroom670@slmails.com>
2025-04-23 22:17:57 -0400

    update zsh with fzf and more

---
+++ modules/home/programs/shell/fzf.nix
@@ -0,0 +1,29 @@
+{ config, lib, ... }:
+let
+  colors = config.lib.stylix.colors.withHashtag;
+  accent = colors.base0D;
+  foreground = colors.base05;
+  muted = colors.base03;
+in {
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    colors = lib.mkForce {
+      "fg+" = accent;
+      "bg+" = "-1";
+      "fg" = foreground;
+      "bg" = "-1";
+      "prompt" = muted;
+      "pointer" = accent;
+      "scrollbar" = muted;
+      "separator" = muted;
+    };
+    defaultOptions = [
+      "-i"
+      "--margin=1"
+      "--layout=reverse"
+      "--border=none"
+      "--info='hidden'"
+    ];
+  };
+}
---
+++ modules/home/programs/shell/zsh.nix
@@ -0,0 +1,83 @@
+{ pkgs, ... }: {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      rs = "sudo nixos-rebuild switch";
+      rsf = "sudo nixos-rebuild switch --flake";
+      rtf = "sudo nixos-rebuild test --flake";
+    };
+
+    history = {
+      size = 10000;
+      save = 10000;
+      ignoreDups = true;
+      ignoreAllDups = true;
+      saveNoDups = true;
+      findNoDups = true;
+      append = true;
+      share = true;
+      ignoreSpace = true;
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "simple";
+    };
+
+    initExtraFirst =
+      "krabby name umbreon -s --no-title"; # | fastfetch --file-raw -";
+
+      initExtra = ''
+        source ~/.p10k.zsh
+        source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+        
+        # Keybindings
+        bindkey -e
+        bindkey '^p' history-search-backward
+        bindkey '^n' history-search-forward
+
+        # Completion styling
+        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+        zstyle ':completion:*' keep-prefix true
+        zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+
+        # fzf-tab
+        # disable sort when completing `git checkout`
+        zstyle ':completion:*:git-checkout:*' sort false
+        # set descriptions format to enable group support
+        # NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+        zstyle ':completion:*:descriptions' format '[%d]'
+        # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+        zstyle ':completion:*' menu no
+        # preview directory's content with eza when completing cd
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+        # custom fzf flags
+        # NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
+        zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+        # To make fzf-tab follow FZF_DEFAULT_OPTS.
+        # NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+        zstyle ':fzf-tab:*' use-fzf-default-opts yes
+        # switch group using `<` and `>`
+        zstyle ':fzf-tab:*' switch-group '<' '>'
+      '';
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+  };
+
+  home.packages = with pkgs; [ 
+    meslo-lgs-nf # for p10k
+    zsh-fzf-tab
+  ];
+}
