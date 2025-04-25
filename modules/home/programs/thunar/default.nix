fd50676d3cca3f03d763ef524ec7a1c79c498ba9
aig77 <git_lunchroom670@slmails.com>
2025-04-25 00:51:56 -0400

    move icon package declaration to themes

--- modules/home/programs/thunar/default.nix
+++ modules/home/programs/thunar/default.nix
@@ -1,10 +1,3 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ xfce.thunar ];
-
-  gtk = {
-    iconTheme = {
-      name = "Papirus";
-      package = pkgs.papirus-icon-theme;
-    };
-  };
 }
--- themes/default.nix
+++ themes/default.nix
@@ -49,5 +49,10 @@
     polarity = "dark";
   };
 
-  home.packages = [ pkgs.papirus-icon-theme ];
+  gtk = {
+    iconTheme = {
+      name = "Papirus";
+      package = pkgs.papirus-icon-theme;
+    };
+  };
 }
