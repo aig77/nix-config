1afdd5f15118876f0966a7a14a3a3c9f7d897803
Arturo <29128253+aig77@users.noreply.github.com>
2025-04-24 21:40:11 -0400

    add x padding

--- modules/home/programs/ghostty/default.nix
+++ modules/home/programs/ghostty/default.nix
@@ -1,1 +1,12 @@
-{ programs.ghostty = { enable = true; }; }
+{ 
+  programs.ghostty = { 
+    enable = true;
+    enableZshIntegration = true;
+    installBatSyntax = true;
+    installVimSyntax = true;
+    settings = {
+      window-padding-x = 10;
+      window-padding-balance = true; 
+    };
+  };
+}
