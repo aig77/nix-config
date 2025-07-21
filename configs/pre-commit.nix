3cfac14aeb76f152466d734e9440232511d5ca3d
Arturo <git.lunchroom670@slmails.com>
2025-07-21 18:42:26 -0400

    minor tweaks to treefmt and pre-commit import syntax

--- configs/pre-commit.nix
+++ configs/pre-commit.nix
@@ -1,10 +1,7 @@
 {
-  hooks = {
+  settings.hooks = {
     alejandra.enable = true;
     deadnix.enable = true;
-    statix = {
-      enable = true;
-      settings.ignore = ["hardware-configuration.nix"];
-    };
+    statix.enable = true;
   };
 }
