634ffd6720e7a4cadde08cf0cd1ad3e49944c46d
Arturo <29128253+aig77@users.noreply.github.com>
2025-08-05 20:06:25 -0400

    fix stylix neovim issues

--- modules/home/programs/neovim/default.nix
+++ modules/home/programs/neovim/default.nix
@@ -13,30 +13,29 @@ in {
 
   stylix.targets.neovim.enable = false;
 
-  # Use home.file trick to write the Stylix theme as Lua
   home.file.".config/nvim/lua/plugins/stylix-loader.lua".text = ''
     return {
       "echasnovski/mini.base16",
-      priority = 1100,
+      priority = 900,
       config = function()
         require('mini.base16').setup({ palette = {
-            base00 = "${colors.base00}",
-            base01 = "${colors.base01}",
-            base02 = "${colors.base02}",
-            base03 = "${colors.base03}",
-            base04 = "${colors.base04}",
-            base05 = "${colors.base05}",
-            base06 = "${colors.base06}",
-            base07 = "${colors.base07}",
-            base08 = "${colors.base08}",
-            base09 = "${colors.base09}",
-            base0A = "${colors.base0A}",
-            base0B = "${colors.base0B}",
-            base0C = "${colors.base0C}",
-            base0D = "${colors.base0D}",
-            base0E = "${colors.base0E}",
-            base0F = "${colors.base0F}",
-          }})
+          base00 = "${colors.base00}",
+          base01 = "${colors.base01}",
+          base02 = "${colors.base02}",
+          base03 = "${colors.base03}",
+          base04 = "${colors.base04}",
+          base05 = "${colors.base05}",
+          base06 = "${colors.base06}",
+          base07 = "${colors.base07}",
+          base08 = "${colors.base08}",
+          base09 = "${colors.base09}",
+          base0A = "${colors.base0A}",
+          base0B = "${colors.base0B}",
+          base0C = "${colors.base0C}",
+          base0D = "${colors.base0D}",
+          base0E = "${colors.base0E}",
+          base0F = "${colors.base0F}",
+        }})
       end
     }
   '';
