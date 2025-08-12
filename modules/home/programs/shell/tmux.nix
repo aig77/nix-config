138fe055d8652d74a23e0f47f43b48febf30dbf1
Arturo <29128253+aig77@users.noreply.github.com>
2025-08-11 23:42:14 -0400

    change fae to fish, zen tmux config, and start opencode config

--- modules/home/programs/shell/tmux.nix
+++ modules/home/programs/shell/tmux.nix
@@ -1,8 +1,33 @@
-{
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     clock24 = true;
     keyMode = "vi";
+    baseIndex = 1;
     mouse = true;
+    sensibleOnTop = true;
+    prefix = "C-space";
+
+    plugins = with pkgs.tmuxPlugins; [
+      vim-tmux-navigator
+      yank
+    ];
+
+    extraConfig = ''
+      set-option -sa terminal-overrides ",xterm*:Tc"
+
+      # Shift Alt vim keys to switch windows
+      bind -n M-H previous-window
+      bind -n M-L next-window
+
+      # Yanking
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+      # Visual indicator for spliting windows
+      bind | split-window -h
+      bind _ split-window -v
+    '';
   };
 }
