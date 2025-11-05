85bf8a4ead0037c98b0fb6bfc19047e25022833c
Arturo Galan <arturo@Arturos-MacBook-Pro.local>
2025-11-05 13:42:58 -0500

    add missing packages

--- hosts/ein/home.nix
+++ hosts/ein/home.nix
@@ -1,17 +1,17 @@
 {
   pkgs,
   config,
+  inputs,
   ...
 }: {
   imports = [
     # Programs
-    ../../modules/home/programs/discord
     ../../modules/home/programs/fetch
     ../../modules/home/programs/ghostty
     ../../modules/home/programs/git
     ../../modules/home/programs/lazygit
     ../../modules/home/programs/neovim
-    ../../modules/home/programs/opencode
+    #../../modules/home/programs/nixcord
     ../../modules/home/programs/shell
     ../../modules/home/programs/spotify
     ../../modules/home/programs/vim
@@ -25,17 +25,11 @@
     homeDirectory = "/Users/" + config.var.username;
 
     packages = with pkgs; [
-      zip
-      unzip
-      xz
-      btop
-      nil
-      nixd
-
-      # backups
       brave
-      #vscodium
-      #zed-editor
+      btop
+      inputs.nix-ai-tools.packages.${pkgs.system}.opencode
+      nil
+      xz
     ];
 
     sessionVariables = {
