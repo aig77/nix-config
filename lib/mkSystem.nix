f2795a6082fb252f1739da8cba8a40cf66d9c0a9
Arturo Galan <arturo@Arturos-MacBook-Pro.local>
2025-11-07 10:46:55 -0500

    mkSystem lib to simplify flake boilerplate

---
+++ lib/mkSystem.nix
@@ -0,0 +1,55 @@
+{inputs, ...}: {
+  mkNixos = {
+    hostname,
+    system ? "x86_64-linux",
+    overlays ? [],
+    extraModules ? [],
+  }:
+    inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {inherit inputs hostname;};
+      modules =
+        [
+          {
+            nixpkgs.config = {
+              allowUnfree = true;
+              allowBroken = true;
+            };
+            nixpkgs.overlays = overlays;
+          }
+          inputs.disko.nixosModules.disko
+          inputs.home-manager.nixosModules.home-manager
+          inputs.stylix.nixosModules.stylix
+          ../hosts/${hostname}/configuration.nix
+        ]
+        ++ extraModules;
+    };
+
+  mkDarwin = {
+    hostname,
+    system ? "aarch64-darwin",
+    overlays ? [],
+    extraModules ? [],
+  }:
+    inputs.darwin.lib.darwinSystem {
+      inherit system;
+      specialArgs = {
+        inherit inputs hostname;
+      };
+      modules =
+        [
+          {
+            nixpkgs.config = {
+              allowUnfree = true;
+              allowBroken = true;
+            };
+            nixpkgs.overlays = overlays;
+          }
+          inputs.home-manager.darwinModules.home-manager
+          inputs.stylix.darwinModules.stylix
+          inputs.nix-homebrew.darwinModules.nix-homebrew
+          ../hosts/${hostname}/configuration.nix
+        ]
+        ++ extraModules;
+    };
+}
