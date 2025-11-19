631fc857c06060a84faf44c044f949b716eb61e1
Arturo <29128253+aig77@users.noreply.github.com>
2025-11-18 19:18:46 -0500

    add zathura

---
+++ modules/home/programs/zathura/default.nix
@@ -0,0 +1,14 @@
+{
+  programs.zathura = {
+    enable = true;
+    options = {
+      recolor = true;
+      recolor-keephue = true;
+      guioptions = "v";
+      adjust-open = "width";
+      statusbar-basename = true;
+      render-loading = false;
+      scroll-step = 120;
+    };
+  };
+}
