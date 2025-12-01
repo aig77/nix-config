f8fe27cd238ae90fade61cdd0216a6778406a5cd
Arturo <git.lunchroom670@slmails.com>
2025-12-01 12:56:05 -0500

    zen kernel + steam apps open fullscreen

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest; # Latest stable kernel
-    # kernelPackages = pkgs.linuxPackages_zen;
+    # kernelPackages = pkgs.linuxPackages_latest; # Latest stable kernel
+    kernelPackages = pkgs.linuxPackages_zen;
 
     loader = {
       grub = {
