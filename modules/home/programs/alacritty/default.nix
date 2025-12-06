65b309aa77779d6c130aca339a8d3359c5f6cc2d
Arturo <git.lunchroom670@slmails.com>
2025-12-05 21:42:20 -0500

    test stylix conditional by system

--- modules/home/programs/alacritty/default.nix
+++ modules/home/programs/alacritty/default.nix
@@ -11,11 +11,11 @@
           x = 10;
           y = 10;
         };
-        opacity = lib.mkForce (
-          if pkgs.stdenv.isDarwin
-          then 1.0
-          else 0.80
-        );
+        # opacity = lib.mkForce (
+        #   if pkgs.stdenv.isDarwin
+        #   then 1.0
+        #   else 0.80
+        # );
       };
     };
   };
---
+++ themes/catppuccin.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (pkgs.stdenv) isDarwin;
+in {
+  stylix = {
+    enable = true;
+    polarity = "dark";
+
+    # https://github.com/tinted-theming/base16-schemes
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrains Mono Nerd Font";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+
+    image = lib.mkIf (!isDarwin) (pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/orangci/walls-catppuccin-mocha/master/minimalist-black-hole.png";
+      sha256 = "sha256-UetLf/3ustmNAXTSoNjqAJKug+ZeMnyf2DMTr+h+eU4=";
+    });
+
+    cursor = lib.mkIf (!isDarwin) {
+      name = "catppuccin-mocha-light-cursors";
+      package = pkgs.catppuccin-cursors.mochaLight;
+      size = 24;
+    };
+
+    opacity = lib.mkIf (!isDarwin) {
+      applications = 0.8;
+      desktop = 0.8;
+      terminal = 0.8;
+      popups = 0.8;
+    };
+  };
+}
