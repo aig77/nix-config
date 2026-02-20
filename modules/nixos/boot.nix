bbef1bdaa92296d6bc1068a4c9fb922683859827
Arturo <29128253+aig77@users.noreply.github.com>
2026-02-19 21:56:34 -0500

    boot rice

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [inputs.grub2-themes.nixosModules.default];
+
   boot = {
     kernelPackages = pkgs.linuxPackages_latest; # Latest stable kernel
     # kernelPackages = pkgs.linuxPackages_zen;
@@ -10,9 +16,40 @@
         useOSProber = true;
         devices = ["nodev"];
       };
+      grub2-theme = {
+        enable = true;
+        theme = "vimix";
+        screen = "2k";
+      };
       efi.canTouchEfiVariables = true;
     };
 
+    plymouth = {
+      enable = true;
+      theme = "lone";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["lone"];
+        })
+      ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 3;
+
     binfmt.emulatedSystems = ["aarch64-linux"];
   };
+
+  stylix.targets.grub.enable = false;
+  stylix.targets.plymouth.enable = false;
 }
