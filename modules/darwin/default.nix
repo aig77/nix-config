f3e1ca6db3f2a9220230a64c2a7a5b1f60269d9e
Arturo <29128253+aig77@users.noreply.github.com>
2026-03-19 00:17:08 -0400

    fix: update darwin secrets path after secrets.yaml moved to modules/

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -56,7 +56,7 @@ _: {
     };
 
     sops = {
-      defaultSopsFile = ../../secrets/secrets.yaml;
+      defaultSopsFile = ../secrets/secrets.yaml;
       defaultSopsFormat = "yaml";
       age.keyFile = "${config.users.users.${config.var.username}.home}/.config/sops/age/keys.txt";
       secrets = {
