abce62bc1e8a53f48eb8739a0d8dc992f12bebfe
Arturo <29128253+aig77@users.noreply.github.com>
2026-03-19 00:02:28 -0400

    chore: move secrets.yaml into modules/secrets/

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -6,7 +6,7 @@ _: {
     ...
   }: {
     sops = {
-      defaultSopsFile = ../../secrets/secrets.yaml;
+      defaultSopsFile = ./secrets.yaml;
       defaultSopsFormat = "yaml";
       age.keyFile = "${config.users.users.${config.var.username}.home}/.config/sops/age/keys.txt";
       secrets = {
