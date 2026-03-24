514069bd49437e60761b3874733c6613b9d6476f
Arturo <29128253+aig77@users.noreply.github.com>
2026-03-23 23:56:33 -0400

    fix: discord networking from nixcord not working correctly, switch to standard discord for the time being

--- modules/discord/nixcord.nix
+++ modules/discord/nixcord.nix
@@ -1,19 +1,24 @@
 _: {
-  flake.modules.homeManager.gui = {inputs, ...}: {
-    imports = [inputs.nixcord.homeModules.nixcord];
-
-    programs.nixcord = {
-      enable = true;
-      config = {
-        frameless = true;
-        plugins = {
-          alwaysAnimate.enable = true;
-          betterFolders.enable = true;
-          fakeNitro.enable = true;
-          imageZoom.enable = true;
-          readAllNotificationsButton.enable = true;
-        };
-      };
-    };
+  flake.modules.homeManager.gui = {pkgs, ...}: {
+    home.packages = [pkgs.discord];
   };
 }
+# _: {
+#   flake.modules.homeManager.gui = {inputs, ...}: {
+#     imports = [inputs.nixcord.homeModules.nixcord];
+#
+#     programs.nixcord = {
+#       enable = true;
+#       config = {
+#         frameless = true;
+#         plugins = {
+#           alwaysAnimate.enable = true;
+#           betterFolders.enable = true;
+#           fakeNitro.enable = true;
+#           imageZoom.enable = true;
+#           readAllNotificationsButton.enable = true;
+#         };
+#       };
+#     };
+#   };
+# }
