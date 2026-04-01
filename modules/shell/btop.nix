b84d0bcabf60710a2341ec250a944b59df3ce527
Arturo <29128253+aig77@users.noreply.github.com>
2026-04-01 00:40:37 -0400

    fix: use btop options for opacity instead of stylix bypassing issue with import in ed

--- modules/shell/btop.nix
+++ modules/shell/btop.nix
@@ -1,11 +1,8 @@
 _: {
-  flake.modules.homeManager.base = {lib, config, ...}:
-    lib.mkMerge [
-      {
-        programs.btop.enable = true;
-      }
-      (lib.mkIf (config.lib ? stylix) {
-        stylix.targets.btop.enable = true;
-      })
-    ];
+  flake.modules.homeManager.base = _: {
+    programs.btop = {
+      enable = true;
+      settings.theme_background = false;
+    };
+  };
 }
