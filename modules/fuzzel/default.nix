bc2bcf4cc3ba5bbdeab3cc891f6b92eed74b4abc
Arturo <29128253+aig77@users.noreply.github.com>
2026-03-31 20:31:06 -0400

    fix: slight ui changes for fuzzel

--- modules/fuzzel/default.nix
+++ modules/fuzzel/default.nix
@@ -13,37 +13,41 @@ _: {
       settings = {
         main = {
           terminal = "${pkgs.${var.terminal}}/bin/${var.terminal}";
-          width = 50;
-          lines = 10;
-          horizontal-pad = 32;
-          vertical-pad = 16;
-          inner-pad = 12;
+          width = 60;
+          lines = 8;
+          horizontal-pad = 40;
+          vertical-pad = 20;
+          inner-pad = 8;
           layer = "overlay";
           font = lib.mkForce "${config.stylix.fonts.monospace.name}:size=14";
           icons-enabled = true;
           icon-theme = config.stylix.icons.dark;
           launch-prefix = "";
-          line-height = 25;
+          line-height = 28;
           filter-desktop = true;
           prompt = ''"󰍉 "'';
+          anchor = "top";
+          y-margin = 480;
+          # hide-before-typing = true;
+          minimal-lines = true;
         };
 
         colors = {
-          background = lib.mkForce "${colors.base00}99";
-          border = lib.mkForce "${colors.base04}ff";
+          background = lib.mkForce "${colors.base00}cc";
+          border = lib.mkForce "${colors.base02}60";
+          text = lib.mkForce "${colors.base04}ff";
+          input = lib.mkForce "${colors.base05}ff";
+          prompt = lib.mkForce "${colors.base03}ff";
+          placeholder = lib.mkForce "${colors.base03}ff";
+          match = lib.mkForce "${colors.base0D}ff";
+          selection = lib.mkForce "${colors.base02}ff";
+          selection-text = lib.mkForce "${colors.base05}ff";
+          selection-match = lib.mkForce "${colors.base0D}ff";
         };
 
         border = {
-          width = 2;
-          radius = 12;
-        };
-
-        shadow = {
-          color = "00000066";
-          count = 1;
-          offset-x = 0;
-          offset-y = 4;
-          size = 20;
+          width = 1;
+          radius = 40;
         };
       };
     };
