3c8b0c99796da4b0e8272f38cf44165b3a438ea6
Arturo <29128253+aig77@users.noreply.github.com>
2026-05-21 21:56:46 -0400

    feat: built faye on new machine, replaced faye's hardware.nix with facter.json

--- modules/hosts/nixos/faye/imports.nix
+++ modules/hosts/nixos/faye/imports.nix
@@ -1,11 +1,14 @@
 {config, ...}: {
-  configurations.nixos.faye.module = {
-    imports = with config.flake.modules.nixos; [
-      base
-      amdgpu
-      htpc
-      steamos
-      no-rgb
-    ];
+  configurations.nixos.faye.module = {inputs, ...}: {
+    facter.reportPath = ./facter.json;
+    imports =
+      [inputs.nixos-facter-modules.nixosModules.facter]
+      ++ (with config.flake.modules.nixos; [
+        base
+        amdgpu
+        htpc
+        steamos
+        no-rgb
+      ]);
   };
 }
