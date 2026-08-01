6d940358f49dd6e8bf6b4dac9e725ff0c938bad3
Arturo <29128253+aig77@users.noreply.github.com>
2026-07-31 23:26:07 -0400

    fix: use command to handle restic database copy so it doesnt copy mid write

--- modules/features/subtrakr.nix
+++ modules/features/subtrakr.nix
@@ -1,11 +1,22 @@
 _: {
-  flake.modules.nixos.subtrakr = {config, ...}: {
+  flake.modules.nixos.subtrakr = {
+    config,
+    pkgs,
+    ...
+  }: {
     var.services.subtrakr = {
       subdomain = "subtrakr";
       port = config.ports.subtrakr;
       public = false;
       auth = false;
-      backup.paths = ["/var/lib/subtrakr"];
+      backup = {
+        paths = ["/var/lib/backups/subtrakr"];
+        prepareCommand = ''
+          mkdir -p /var/lib/backups/subtrakr
+          ${pkgs.sqlite}/bin/sqlite3 /var/lib/subtrakr/subtrakr.db \
+            ".backup '/var/lib/backups/subtrakr/subtrakr.db'"
+        '';
+      };
     };
 
     virtualisation = {
