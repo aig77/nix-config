5da3567f8f9a85929690bd78a54053da52bd7d8a
Arturo <29128253+aig77@users.noreply.github.com>
2026-08-03 11:12:39 -0400

    feat(monitoring): monitoring for open web ui

--- modules/features/gatus.nix
+++ modules/features/gatus.nix
@@ -171,6 +171,16 @@
                 - type: discord
                   failure-threshold: 2
                   success-threshold: 1
+            - name: Open WebUI
+              url: http://localhost:${toString config.ports.open-webui}/health
+              interval: 5m
+              conditions:
+                - "[STATUS] == 200"
+                - "[BODY].status == true"
+              alerts:
+                - type: discord
+                  failure-threshold: 2
+                  success-threshold: 1
             - name: Blocky DNS
               url: tcp://192.168.68.101:53
               interval: 1m
