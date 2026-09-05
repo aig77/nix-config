8f742ea96521a6c48d6af602133cfb0e97a78d6a
Arturo <29128253+aig77@users.noreply.github.com>
2026-09-04 22:13:56 -0400

    fix: cachix for noctalia

--- modules/aspects/nix.nix
+++ modules/aspects/nix.nix
@@ -16,6 +16,7 @@ _: {
           "https://hyprland.cachix.org"
           "https://niri.cachix.org"
           "https://nix-community.cachix.org"
+          "https://noctalia.cachix.org"
           "https://numtide.cachix.org"
         ];
         trusted-public-keys = [
@@ -24,6 +25,7 @@ _: {
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
           "niri.cachix.org-1:Wv0Om60S7fSdV8Zk3SAnR1BkeE7CidN9sMny03u0T88="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
           "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
         ];
       };
